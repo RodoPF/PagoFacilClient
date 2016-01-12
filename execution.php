bfb262ac92d5f9d8e3104460c1bab2da1b51e52c
Rodolfo Solorzano <rodo@pagofacil.net>
2016-01-12 10:02:25 -0600

    Cliente completo

---
+++ execution.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'lib/vendor/me/sender.php';
+$test = new Rest_Json_Sender();
+$test->type = 'get';
+$test->params = array(
+	'nombre'=>'Juan',
+	'apellidos'=>'Lopez',
+	'numeroTarjeta'=>5579567890123456,
+	'cvt'=>123,
+	'cp'=>11560,
+	'mesExpiracion'=>10,
+	'anyoExpiracion'=>16,
+	'monto'=>20.00,
+	'idSucursal'=>'e147ee31531d815e2308d6d6d39929ab599deb98',
+	'idUsuario'=>'f541b3f11f0f9b3fb33499684f22f6d711f2af58',
+	'idServicio'=>3,
+	'email'=>'rodo@pagofacil.net',
+	'telefono'=>5512345678,
+	'celular'=>5587654321,
+	'calleyNumero'=>'Anatole France 311',
+	'colonia'=>'Polanco',
+	'municipio'=>'Miguel Hidalgo',
+	'estado'=>'Distrito Federal',
+	'pais'=>'Mexico',
+	'param1'=>'param1',
+	'param2'=>'param2',
+	'param3'=>'param3',
+	'param4'=>'param4',
+	'param5'=>'param5',
+	'noMail'=>1
+);
+$test->send();
+echo $test->getResult();
+?>
---
+++ lib/vendor/me/sender.php
@@ -0,0 +1,104 @@
+<?php
+
+abstract class Base_Sender{
+	public $params;
+	public $type;
+	protected $host;
+	protected $result;
+	
+	protected function prepare_params(){
+		return http_build_query($this->params);
+	}
+	
+	protected function send_post(){
+		$conn = curl_init();
+		curl_setopt($conn, CURLOPT_URL, $this->host);
+		curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($conn, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($conn, CURLOPT_POST, TRUE);
+		curl_setopt($conn, CURLOPT_POSTFIELDS, $this->prepare_params());
+		$result = curl_exec($conn);
+		curl_close($conn);
+		return $result;
+	}
+	
+	protected function send_get(){
+		$conn = curl_init();
+		curl_setopt($conn, CURLOPT_URL, $this->host.'?'.$this->prepare_params());
+		curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($conn, CURLOPT_RETURNTRANSFER, TRUE);
+		$result = curl_exec($conn);
+		curl_close($conn);
+		return $result;
+	}
+	
+	protected function send_put(){
+		$conn = curl_init();
+		curl_setopt($conn, CURLOPT_URL, $this->host);
+		curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($conn, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($conn, CURLOPT_CUSTOMREQUEST, 'PUT');
+		curl_setopt($conn, CURLOPT_POSTFIELDS, $this->prepare_params());
+		$result = curl_exec($conn);
+		curl_close($conn);
+		return $result;
+	}
+	
+	public function send(){
+		$this->result = call_user_func(array($this, 'send_'.$this->type));
+	}
+	
+	public function getResult(){
+		$result = '<pre>';
+		$result.= print_r($this->result, TRUE);
+		$result.= '</pre>';
+		return $result;
+	}
+}
+
+class Rest_XML_Sender extends Base_Sender {
+	protected $host = 'http://pagofacil.local/public/Wsrtransaccion/';
+	protected function prepare_params(){
+		$params = array(
+			'method'=>'transaccion',
+			'data'=>$this->params
+		);
+		return http_build_query($params);
+	}
+	public function getResult(){
+		$result = '<xmp>';
+		$result.= print_r($this->result, TRUE);
+		$result.= '</xmp>';
+		return $result;
+	}
+}
+
+class Rest_Json_Sender extends Base_Sender {
+	protected $host = 'http://pagofacil.local/public/Wsrtransaccion/index/format/json';
+	protected function prepare_params(){
+		$params = array(
+			'method'=>'transaccion',
+			'data'=>$this->params
+		);
+		return http_build_query($params);
+	}
+}
+
+class Soap_Sender extends Base_Sender {
+	protected $wsdl = 'http://pagofacil.local/public/Wsstransaccion/?wsdl';
+	
+	protected function send_soap(){
+		$client = new SoapClient($this->wsdl);
+		$result = $client->transaccion($this->params);
+		return $result;
+	}
+	
+	public function send(){
+		$this->result = $this->send_soap();
+	}
+	
+}
+
+class Form_Sender extends Base_Sender {
+	protected $host = 'http://pagofacil.local/public/Payform';
+}
